0061d483f2561f1aa29a56ea1bc6904dc7563c99
Junho <junho@jieyou.com>
2013-11-23 05:31:39 +0800

    use mud

---
+++ mymud/lib/include/globals.h
@@ -0,0 +1,6 @@
+//User IDs
+#define ROOT_UID        "Root"
+#define BACKBONE_UID    "Backbone"
+
+#define LOGIN_OB		"/object/login"
+
---
+++ mymud/lib/single/master.c
@@ -0,0 +1,22 @@
+string get_root_uid()
+{
+   return ROOT_UID;
+}
+
+string get_bb_uid()
+{
+   return BACKBONE_UID;
+}
+
+object connect(int port)
+{
+    object login_ob;
+    login_ob = new(LOGIN_OB);
+    return login_ob;
+}
+
+string creator_file(string filename)
+{
+    return ROOT_UID;
+}
+
